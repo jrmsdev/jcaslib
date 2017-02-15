0b743697c1bc8536e851e4d300f11653ae1841a0
Jeremías <jrmsdev@gmail.com>
2017-02-15 20:54:44 -0300

    remove non standard install args

--- vars.mk
+++ vars.mk
@@ -3,5 +3,5 @@ CFLAGS += -Wall -pedantic -std=c11 $(JCLIB_DEBUG)
 
 PREFIX ?= /opt/pkg
 
-INSTALL_EXE = install -v -m 0755 -b -B .old
-INSTALL_F = install -v -m 0644 -b -B .old
+INSTALL_EXE = install -v -m 0755 -b
+INSTALL_F = install -v -m 0644 -b
