250a4577e5c05b2c278c55f399c6190145055c9f
Jeremías <jrmsdev@gmail.com>
2017-02-15 05:25:13 -0300

    read path items from cmd args

--- examples/path/path_join.c
+++ examples/path/path_join.c
@@ -4,10 +4,23 @@
 #include <err.h>
 #include <jclib/path.h>
 
-int main()
+int main(int argc, char *argv[])
 {
     path_type p;
+    memset(p, '\0', PATH_MAX_LEN);
+
     path_join(p, 4, "/etc", "1", "2", "3");
-    printf("p: '%s'\n", p);
+    printf("path: '%s'\n", p);
+
+    if (argc > 1)
+    {
+        memset(p, '\0', PATH_MAX_LEN);
+        for (int idx = 1; idx < argc; idx++)
+        {
+            path_join(p, 2, p, argv[idx]);
+       }
+       printf("path: '%s'\n", p);
+    }
+
     return 0;
 }
