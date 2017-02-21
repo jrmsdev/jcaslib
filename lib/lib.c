5b62111fbb51723ec78ef3335a08256d23575fd5
Jeremías <jrmsdev@gmail.com>
2017-02-21 17:43:08 -0300

    do not memset at xmalloc

--- lib/lib.c
+++ lib/lib.c
@@ -1,13 +1,11 @@
 #include <jclib/lib.h>
 #include <err.h>
-#include <string.h>
 
 void * xmalloc (size_t size)
 {
     void *p = malloc (size);
     if (p == NULL)
         err(1, "could not malloc");
-    memset (p, 0, size);
     return (p);
 }
 
