08936ce18ed306705b5861090a87c53cf9d71195
Jeremías <jrmsdev@gmail.com>
2017-02-21 20:49:47 -0300

    fix str alloc

--- lib/str_alloc.c
+++ lib/str_alloc.c
@@ -1,13 +1,11 @@
 #include <jclib/str.h>
 #include <jclib/lib.h>
-#include <string.h>
 
 str_type * str_alloc ()
 {
-    str_type *s = (str_type *) xmalloc (sizeof (str_type *));
+    str_type *s = (str_type *) xmalloc (sizeof (str_type));
+    s->allocated = STR_ALLOC * sizeof (char);
     s->len = 0;
-    s->allocated = STR_ALLOC;
-    s->data = (char *) xmalloc (STR_ALLOC);
-    memset (s->data, 0, STR_ALLOC);
+    s->data = (char *) xmalloc (s->allocated);
     return s;
 }
