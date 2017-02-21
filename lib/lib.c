ba614932347abdbe4be82cefaf326d5468171f98
Jeremías <jrmsdev@gmail.com>
2017-02-21 20:49:05 -0300

    bzero allocated space

--- lib/lib.c
+++ lib/lib.c
@@ -1,11 +1,13 @@
 #include <jclib/lib.h>
 #include <err.h>
+#include <string.h>
 
 void * xmalloc (size_t size)
 {
     void *p = malloc (size);
     if (p == NULL)
         err(1, "could not malloc");
+    bzero (p, size);
     return (p);
 }
 
