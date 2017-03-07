e90bb5d06b6dfbccc278f660412e23b64d2bdbde
Jeremías <jrmsdev@gmail.com>
2017-03-07 00:13:49 -0300

    str_realloc tests

--- lib/str/str_alloc_t.c
+++ lib/str/str_alloc_t.c
@@ -25,9 +25,33 @@ str_alloc_t (void)
 }
 
 
+void
+str_realloc_t (void)
+{
+    t_start ("str_realloc");
+    str_type *s = str_alloc ();
+
+    t_log ("sizeof -> %zu\n", sizeof (*s));
+    t_check (EQ (sizeof (*s), 24), "sizeof != 24");
+
+    t_log ("STR_ALLOC -> %d\n", STR_ALLOC);
+    t_log ("allocated -> %zu\n", s->allocated);
+    t_check (EQ (s->allocated, STR_ALLOC), "allocated != STR_ALLOC");
+
+    str_realloc (s);
+    t_log ("reallocated -> %zu\n", s->allocated);
+    t_check (EQ (s->allocated, (STR_ALLOC * 2)),
+                    "reallocated != (STR_ALLOC * 2)");
+
+    str_free (s);
+    t_end ("str_realloc");
+}
+
+
 int
 main (void)
 {
     str_alloc_t ();
+    str_realloc_t ();
     return (0);
 }
