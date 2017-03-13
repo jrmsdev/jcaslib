52884bdc60afc934e2fb0dec1024543465ac1809
Jeremías <jrmsdev@gmail.com>
2017-03-13 01:24:34 -0300

    remove write perm when installing

--- mk/vars.mk
+++ mk/vars.mk
@@ -5,8 +5,8 @@ CFLAGS += -Wall -Wextra -pedantic -pedantic-errors -std=c11 $(JCLIB_DEBUG)
 PREFIX ?= /opt/pkg
 
 INSTALL_BACKUP ?=
-INSTALL_EXE = install -v -m 0755 $(INSTALL_BACKUP)
-INSTALL_F = install -v -m 0644 $(INSTALL_BACKUP)
+INSTALL_EXE = install -v -m 0555 $(INSTALL_BACKUP)
+INSTALL_F = install -v -m 0444 $(INSTALL_BACKUP)
 
 .SUFFIXES:
 .SUFFIXES: .c .o
