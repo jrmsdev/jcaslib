6dbbfd905c9b1bdf9b9cd993d8115bc28f83a1b6
Jeremías <jrmsdev@gmail.com>
2017-03-13 01:58:26 -0300

    use /usr/local as default PREFIX

--- mk/vars.mk
+++ mk/vars.mk
@@ -2,7 +2,7 @@ SH = /bin/sh
 JCLIB_DEBUG ?=
 CFLAGS += -Wall -Wextra -pedantic -pedantic-errors -std=c11 $(JCLIB_DEBUG)
 
-PREFIX ?= /opt/pkg
+PREFIX ?= /usr/local
 
 INSTALL_BACKUP ?=
 INSTALL_EXE = install -v -m 0555 $(INSTALL_BACKUP)
