69b773e558b039a70ce5b74ddf4d5f27f6ec5ce3
Jeremías <jrmsdev@gmail.com>
2017-03-16 20:49:48 -0300

    remove debug code

--- examples/str/str_array_ex.c
+++ examples/str/str_array_ex.c
@@ -6,29 +6,12 @@ int
 main (void)
 {
 	str_array_type *arr = str_array_alloc ();
-	printf ("array size: %zu\n", str_array_size (arr));
-	printf ("array 0 len: %zu\n", str_array_len (arr));
-	printf ("array 0 last: %zu\n", str_array_last (arr));
 
 	str_array_insert (arr, 0, "lalala0");
-	printf ("array 1 len: %zu\n", str_array_len (arr));
-	printf ("array 1 last: %zu\n", str_array_last (arr));
-
 	str_array_insert (arr, 1, "lalala1");
-	printf ("array 2 len: %zu\n", str_array_len (arr));
-	printf ("array 2 last: %zu\n", str_array_last (arr));
-
 	str_array_append (arr, "lalala2");
-	printf ("array 3 len: %zu\n", str_array_len (arr));
-	printf ("array 3 last: %zu\n", str_array_last (arr));
-
 	str_array_insert (arr, 5, "lalala5");
-	printf ("array 4 len: %zu\n", str_array_len (arr));
-	printf ("array 4 last: %zu\n", str_array_last (arr));
-
 	str_array_append (arr, "lalala6");
-	printf ("array 5 len: %zu\n", str_array_len (arr));
-	printf ("array 5 last: %zu\n", str_array_last (arr));
 
     for (size_t i = 0; i <= str_array_last (arr); i++)
     {
