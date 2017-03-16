903792b4749609e9c9a9d1164973247432670869
Jeremías <jrmsdev@gmail.com>
2017-03-16 02:58:45 -0300

    do not realloc array if len is zero

--- lib/str/str_array_realloc.c
+++ lib/str/str_array_realloc.c
@@ -4,13 +4,16 @@
 void
 str_array_realloc (str_array_type *arr)
 {
-	size_t newsize = str_array_size (arr)
-			+ (STR_ARRAY_ALLOC * sizeof (str_type *));
-	str_type **d = (str_type **) xrealloc (arr->data, newsize);
-	arr->data = d;
-	arr->allocated = newsize;
-	for (size_t i = str_array_len (arr) + STR_ARRAY_ALLOC; i < newsize; i++)
+	if (str_array_len (arr) > 0)
 	{
-		arr->data[i] = NULL;
+		size_t newsize = str_array_size (arr)
+				+ (STR_ARRAY_ALLOC * sizeof (str_type *));
+		str_type **d = (str_type **) xrealloc (arr->data, newsize);
+		arr->data = d;
+		arr->allocated = newsize;
+		for (size_t i = str_array_len (arr) + STR_ARRAY_ALLOC; i < newsize; i++)
+		{
+			arr->data[i] = NULL;
+		}
 	}
 }
