0ce6f6cabc0ebd32cd471f665d1da384c2146191
Jeremías <jrmsdev@gmail.com>
2017-03-15 23:12:47 -0300

    fix setting allocated size

--- lib/str/str_realloc.c
+++ lib/str/str_realloc.c
@@ -5,8 +5,9 @@
 void
 str_realloc (str_type *s)
 {
-    char *d = (char *) xrealloc (s->data, str_size (s) + STR_ALLOC);
-    s->allocated += STR_ALLOC;
+    char *d = (char *) xrealloc (s->data,
+			str_size (s) + (STR_ALLOC * sizeof (char)));
+    s->allocated += (STR_ALLOC * sizeof (char));
     s->data = d;
     bzero (s->data + s->len, str_size (s) - s->len);
 }
