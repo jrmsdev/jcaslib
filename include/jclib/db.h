990597e6d291583941cd1d7d205989e22ae2456f
Jeremías <jrmsdev@gmail.com>
2017-03-17 18:00:27 -0300

    get rid of dbm lib code (broken state)

--- include/jclib/db.h
+++ include/jclib/db.h
@@ -1,12 +1,3 @@
 #ifndef JCLIB_DB_H
 #define JCLIB_DB_H
-
-#ifdef USE_GDBM
-#  include <gdbm-ndbm.h>
-#else
-#  include <ndbm.h>
-#endif
-
-extern DBM * db_openfile (const char *filename);
-
 #endif /* JCLIB_DB_H */
