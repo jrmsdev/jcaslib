eda3a48a31f4fa7f9e93a9d0f7764d509551f1af
Jeremías <jrmsdev@gmail.com>
2017-03-18 06:59:30 -0300

    do not use strndup

--- lib/db/_datum_alloc.c
+++ lib/db/_datum_alloc.c
@@ -8,6 +8,7 @@ _datum_alloc (const char *str)
 	size_t slen = strlen (str) + 1;
 	datum *d = (datum *) xmalloc (sizeof (datum));
 	d->dsize = slen;
-	d->dptr = (void *) strndup (str, slen);
+	d->dptr = (void *) xmalloc (slen * (sizeof (char)));
+	strncpy (d->dptr, str, slen);
 	return (d);
 }
