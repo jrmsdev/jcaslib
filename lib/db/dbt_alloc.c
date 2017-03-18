85119010d9b67535b120a9db18674ff3665cdf9c
Jeremías <jrmsdev@gmail.com>
2017-03-18 02:13:56 -0300

    fix dbt size

--- lib/db/dbt_alloc.c
+++ lib/db/dbt_alloc.c
@@ -5,7 +5,7 @@
 DBT *
 dbt_alloc (const char *str)
 {
-	size_t slen = strlen (str);
+	size_t slen = strlen (str) + 1;
 	DBT *t = (DBT *) xmalloc (sizeof (DBT));
 	t->size = slen;
 	t->data = (void *) strndup (str, slen);
