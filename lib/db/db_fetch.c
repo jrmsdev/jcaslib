b9d80f5fd43001f1e38b7308c371b4a7e9a7b714
Jeremías <jrmsdev@gmail.com>
2017-03-18 20:08:35 -0300

    db fetch return char*

--- lib/db/db_fetch.c
+++ lib/db/db_fetch.c
@@ -1,18 +1,15 @@
 #include <jclib/db.h>
 
-str_type *
+char *
 db_fetch (DBM *db, const char *key)
 {
     datum *_key = _datum_alloc (key);
-    datum _val = dbm_fetch (db, *_key);
-    if (_val.dptr == NULL)
+    datum val = dbm_fetch (db, *_key);
+    if (val.dptr == NULL)
     {
         _datum_free (_key);
         return (NULL);
     }
-    str_type *val = str_alloc();
-    str_nset (val, _val.dptr, _val.dsize);
     _datum_free (_key);
-    _datum_free (&_val);
-    return (val);
+    return ((char *) val.dptr);
 }
