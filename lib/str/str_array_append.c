3bf0bbfe0bdfd0a9fbdf7bb873b683bc40bca6c8
Jeremías <jrmsdev@gmail.com>
2017-03-18 19:18:22 -0300

    fix str array append

--- lib/str/str_array_append.c
+++ lib/str/str_array_append.c
@@ -3,6 +3,8 @@
 void
 str_array_append (str_array_type *arr, const char *str)
 {
-    size_t idx = arr->last + 1;
+    size_t idx = 0;
+    if (arr->len > 0)
+        idx = arr->last + 1;
     str_array_insert (arr, idx, str);
 }
