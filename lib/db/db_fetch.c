de5aac3097c25c0f13b00d90f18522e57fee8dab
Jeremías <jrmsdev@gmail.com>
2017-03-18 19:43:52 -0300

    db fetch free datums

--- lib/db/db_fetch.c
+++ lib/db/db_fetch.c
@@ -6,8 +6,13 @@ db_fetch (DBM *db, const char *key)
     datum *_key = _datum_alloc (key);
     datum _val = dbm_fetch (db, *_key);
     if (_val.dptr == NULL)
+    {
+        _datum_free (_key);
         return (NULL);
+    }
     str_type *val = str_alloc();
     str_nset (val, _val.dptr, _val.dsize);
+    _datum_free (_key);
+    _datum_free (&_val);
     return (val);
 }
