927dce4c8abdb99644ff07231679638dcf166097
Jeremías <jrmsdev@gmail.com>
2017-03-19 19:06:27 -0300

    get rid of scripts/ci

--- scripts/ci/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -e
-./configure --use-gdbm
-bmake
-bmake examples
-bmake dist
-exit 0
--- scripts/ci/install-deps.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-set -e
-export DEBIAN_FRONTEND="noninteractive"
-apt-get update && apt-get install -y bmake libgdbm-dev
-exit 0
