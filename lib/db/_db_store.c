92137ea102799d8d0f5dae303a9aa2684ed8e504
Jeremías <jrmsdev@gmail.com>
2017-03-20 20:36:05 -0300

    db last update epoch

--- lib/db/_db_store.c
+++ lib/db/_db_store.c
@@ -1,5 +1,11 @@
 #include <jclib/db.h>
 #include <jclib/log.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define DB_UPDATE_KEY DB_KEY_PREFIX"update"
+
+static void _db_updated ();
 
 int
 _db_store (DBM *db, const char *key, const char *val, int flags)
@@ -20,6 +26,23 @@ _db_store (DBM *db, const char *key, const char *val, int flags)
         /* not reached */
         return (stat);
     }
+    else if (stat == 0)
+    {
+        _db_updated (db);
+    }
 
     return (stat);
+}
+
+void
+_db_updated (DBM *db)
+{
+    time_t update_t = time (NULL);
+    if (update_t == -1)
+        log_exit (1, "could not get current epoch");
+
+    char *update_s;
+    asprintf (&update_s, "%ld", update_t);
+    db_insert (db, DB_UPDATE_KEY, update_s);
+    free (update_s);
 }
