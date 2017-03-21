7340520d65b055932be0396b8bad5429dc4a9a10
Jeremías <jrmsdev@gmail.com>
2017-03-20 21:25:12 -0300

    getprogname is not portable to linux

--- lib/log/log.c
+++ lib/log/log.c
@@ -1,15 +1,9 @@
 #include <jclib/log.h>
-#include <stdlib.h>
 
 int _logfp(FILE *fp, const char *tag, char *fmt, va_list ap) {
     int ret;
     if (tag != NULL)
-    {
         fprintf (fp, "%s: ", tag);
-        const char *prog = getprogname ();
-        if (prog != NULL)
-            fprintf (fp, "%s: ", prog);
-    }
     ret = vfprintf(fp, fmt, ap);
     va_end(ap);
     ret += fprintf(fp, "%s\n", "");
