a2ff919605e092ea4517cc3b7896531993cae541
Jeremías <jrmsdev@gmail.com>
2017-03-22 01:45:14 -0300

    log print progname if set

--- include/jclib/log.h
+++ include/jclib/log.h
@@ -4,11 +4,12 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-int _logfp (FILE *fp, const char *tag, char *fmt, va_list ap);
 extern int log_print (char *fmt, ...);
 extern int log_dbg (char *fmt, ...);
 extern int log_err (char *fmt, ...);
 extern int log_warn (char *fmt, ...);
 extern void log_exit (int stat, char *fmt, ...);
+extern void log_set_progname (char *name);
+int _logfp (FILE *fp, const char *tag, char *fmt, va_list ap);
 
 #endif /* JCLIB_LOG_H */
--- lib/log/log.c
+++ lib/log/log.c
@@ -1,11 +1,24 @@
 #include <jclib/log.h>
+#include <libgen.h>
+
+static char *_progname = NULL;
 
 int _logfp(FILE *fp, const char *tag, char *fmt, va_list ap) {
     int ret;
     if (tag != NULL)
+    {
         fprintf (fp, "%s: ", tag);
+        if (_progname != NULL)
+            fprintf (fp, "%s: ", basename (_progname));
+    }
     ret = vfprintf(fp, fmt, ap);
     va_end(ap);
     ret += fprintf(fp, "%s\n", "");
     return ret;
+}
+
+void
+log_set_progname (char *name)
+{
+    _progname = name;
 }
