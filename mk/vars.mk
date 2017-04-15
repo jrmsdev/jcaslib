a83d2df87267dcce722502f2a1cf9d0bcd0ae8eb
Jeremías <jrmsdev@gmail.com>
2017-04-15 02:31:46 -0300

    clean and rebuild everything with coverage support

--- mk/vars.mk
+++ mk/vars.mk
@@ -1,5 +1,10 @@
 SH = /bin/sh
 JCASLIB_DEBUG ?=
+
+.ifdef TEST_COVERAGE
+JCASLIB_DEBUG += -coverage -O0
+.endif
+
 CFLAGS += -Wall -Wextra -pedantic -pedantic-errors -std=c11 $(JCASLIB_DEBUG)
 
 PREFIX ?= /opt/pkg
@@ -7,3 +12,6 @@ PREFIX ?= /opt/pkg
 INSTALL_BACKUP ?=
 INSTALL_EXE = install -v -m 0555 $(INSTALL_BACKUP)
 INSTALL_F = install -v -m 0444 $(INSTALL_BACKUP)
+
+GCOV_CC ?= gcc
+GCOV_CMD ?= gcov5
