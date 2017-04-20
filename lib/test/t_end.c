ed3ca03eaa33ed57ed1b9221ef000879bd96b916
Jeremías <jrmsdev@gmail.com>
2017-04-20 05:12:13 -0300

    fail if run or expect is zero too

--- lib/test/t_end.c
+++ lib/test/t_end.c
@@ -5,15 +5,28 @@ static void free_log (test_T *t);
 static void show_log (test_T *t);
 static void free_t (test_T *t);
 
+
 void
 t_end (test_T *t)
 {
     t->ts->run++;
-    if (EQ (t->run, t->expect) != 0)
+
+    if (t->expect == 0)
+    {
+        _tfail (t, "expects not to run any check");
+        t->failed++;
+    }
+    else if (t->run == 0)
+    {
+        _tfail (t, "no checks ran");
+        t->failed++;
+    }
+    else if (EQ (t->run, t->expect) != 0)
     {
         _tfail (t, "check(s) run(%d) != expect(%d)", t->run, t->expect);
         t->failed++;
     }
+
     if (t->failed == 0)
     {
         _tpass (t, "%d/%d check(s)", t->run, t->expect);
@@ -25,9 +38,11 @@ t_end (test_T *t)
         show_log (t);
         t->ts->failed += t->failed;
     }
+
     free_log (t);
     free_t (t);
 }
+
 
 void
 free_log (test_T *t)
@@ -41,12 +56,14 @@ free_log (test_T *t)
     t->log = NULL;
 }
 
+
 void
 show_log (test_T *t)
 {
     for (size_t i = 0; i < t->loglines; i++)
         _tinfo (t, "%s", t->log[i]);
 }
+
 
 void
 free_t (test_T *t)
