6093c9b968ae05567c235df4e27563e3f930f772
Jeremías <jrmsdev@gmail.com>
2017-04-19 21:27:52 -0300

    redo-makefile - initial commit

---
+++ mk1/build.libmod.mk
@@ -0,0 +1,9 @@
+SH = /bin/sh
+
+.PHONY: build
+build:
+	@echo "build.libmod: $(PWD)"
+
+.PHONY: clean
+clean:
+	@echo "clean.libmod PWD: $(PWD)"
---
+++ mk1/inc.libmod.mk
@@ -0,0 +1,14 @@
+SH = /bin/sh
+MODDIR := $(PWD)
+SRCDIR != realpath $(MODDIR)/../..
+MODNAME != basename $(MODDIR)
+BUILDDIR := $(SRCDIR)/build1/lib/$(MODNAME)
+
+.PHONY: build
+build:
+	@mkdir -vp $(BUILDDIR)
+	$(MAKE) -C $(BUILDDIR) -f $(SRCDIR)/mk1/build.libmod.mk build
+
+.PHONY: clean
+clean:
+	@rm -rfv $(BUILDDIR)
