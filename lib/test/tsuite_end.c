1b6f6021508384c3ab94718c3b232a24d5597aaf
Jeremías <jrmsdev@gmail.com>
2017-04-20 05:08:57 -0300

    fail if run or expect is zero

--- lib/test/tsuite_end.c
+++ lib/test/tsuite_end.c
@@ -8,7 +8,17 @@ tsuite_end (test_suite_T *ts)
 {
     int stat = ts->failed;
 
-    if (EQ (ts->run, ts->expect) != 0)
+    if (ts->expect == 0)
+    {
+        printf (_COLUMS_FORMAT, "[FAIL]", ts->name, "expects not to run any test\n");
+        stat++;
+    }
+    else if (ts->run == 0)
+    {
+        printf (_COLUMS_FORMAT, "[FAIL]", ts->name, "no tests ran\n");
+        stat++;
+    }
+    else if (EQ (ts->run, ts->expect) != 0)
     {
         printf (_COLUMS_FORMAT, "[FAIL]", ts->name, "tests count");
         printf (" run(%d) != expect(%d)\n", ts->run, ts->expect);
