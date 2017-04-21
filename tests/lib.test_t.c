fd5170ac008e61991ec8991f5404cd7d790bc4a5
Jeremías <jrmsdev@gmail.com>
2017-04-21 02:11:21 -0300

    include files so are shown in coverage reports

--- tests/lib.test_t.c
+++ tests/lib.test_t.c
@@ -1,12 +1,24 @@
 #include <jcaslib/test.h>
+#include "../lib/test/_tfail.c"
+#include "../lib/test/_tinfo.c"
+#include "../lib/test/_tpass.c"
+#include "../lib/test/_tprint.c"
+#include "../lib/test/t_check.c"
+#include "../lib/test/t_debug.c"
+#include "../lib/test/t_end.c"
+#include "../lib/test/t_error.c"
+#include "../lib/test/t_log.c"
+#include "../lib/test/t_fatal.c"
+#include "../lib/test/t_start.c"
+#include "../lib/test/tsuite_end.c"
 #include "../lib/test/tsuite_start.c"
 
 
 void
-tsuite_start_t (test_suite_T *ts)
+fake_t (test_suite_T *ts)
 {
     int expect = 1;
-    test_T *t = t_start (ts, "tsuite_start", expect);
+    test_T *t = t_start (ts, "fake", expect);
 
     t_check (t, 0, "");
 
@@ -23,7 +35,7 @@ main (int argc, char *argv[])
     int expect = 1;
     test_suite_T *ts = tsuite_start (argv[0], expect);
 
-    tsuite_start_t (ts);
+    fake_t (ts);
 
     return (tsuite_end (ts));
 }
