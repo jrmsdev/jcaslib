ba9b07e058c3d64a889c6d5f70711306fc1448aa
Jeremías <jrmsdev@gmail.com>
2017-05-20 21:18:18 -0300

    use os_lsdir

--- share/license-header.c
+++ share/license-header.c
@@ -1,30 +1,55 @@
 /* license-header.c */
 
-#include <jcaslib/os.h>
+#define _WITH_GETLINE
 
+#include <jcaslib/os.h>
+#include <jcaslib/path.h>
+
+#include <err.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+
+char *scan_dirname = NULL;
+char *filename_ext = NULL;
+size_t filename_ext_len = 0;
+
+static void
+parse_args (int argc, char *argv[])
+{
+    if (argc < 3)
+        errx (1, "invalid number of parameters");
+    scan_dirname = argv[1];
+    filename_ext = argv[2];
+    filename_ext_len = strlen (filename_ext);
+}
+
+
+static void
+check_file (char *fname)
+{
+    char *fext = path_filename_ext (fname);
+    printf ("check: %s (%s)", fname, fext);
+
+    if (memcmp (filename_ext, fext, filename_ext_len) == 0)
+        printf (" OK");
+
+    printf ("\n");
+}
+
 
 int
-main (void)
+main (int argc, char *argv[])
 {
-    struct dirent **dlist;
-    int entries = os_lsdir (&dlist, ".");
-
-    printf ("dlist entries: %d\n", entries);
-
-    for (int idx = 0; idx < entries; idx++)
+    str_array_type *dlist = str_array_alloc ();
+    parse_args (argc, argv);
+    printf ("scandir:'%s' file_ext:'%s'\n", scan_dirname, filename_ext);
+    os_lsdir (dlist, scan_dirname, 0);
+    for (size_t i = 0; i < str_array_len (dlist); i++)
     {
-        struct dirent *f = dlist[idx];
-        printf ("f->d_name: %s", f->d_name);
-        if (os_isdir (f->d_name))
-        {
-            printf (" (dir)");
-        }
-        printf ("\n");
-        free (dlist[idx]);
+        str_type *fname = str_array_get (dlist, i);
+        check_file (str_get (fname));
     }
-
-    free (dlist);
-    return (0);
+    str_array_free (dlist);
 }
