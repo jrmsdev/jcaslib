c8397b8d8eccedca5671a3237cc5250890f12b8c
Jeremías <jrmsdev@gmail.com>
2017-05-22 00:56:42 -0300

    improve gcov includes_t files generation

--- scripts/gcov-includes.py
+++ scripts/gcov-includes.py
@@ -10,37 +10,13 @@ if __name__ == '__main__':
     src_files.extend (glob ('../bin/*/*.c'))
     src_files.extend (glob ('../lib/*/*.c'))
 
-    print ('#include "includes_t.h"')
-    print ()
-
     included_files = 0
     for f in src_files:
         if path.basename (f) != 'main.c':
             print ('#include "{}"'.format (f))
             included_files += 1
 
-    print ()
-    print ('#include <jcaslib/test.h>')
-    print ()
-
-    print ('void includes_t (test_suite_T *ts) {')
-    print ('  int expect = 1;')
-    print ('  test_T *t = t_start (ts, "{:d} files", expect);'.format (
-           included_files))
-    print ('  t_check (t, 0, "");')
-    print ('  t_end (t);')
-    print ('}')
-    print ()
-
-    print ('int main (int argc, char *argv[]) {')
-    print ('  if (argc < 1)')
-    print ('    errx (1, "ERROR: argc < 1???");')
-    print ('  int expect = 1;')
-    print ('  test_suite_T *ts = tsuite_start (argv[0], expect);')
-    print ('  includes_t (ts);')
-    print ('  return (tsuite_end (ts));')
-    print ('}')
-
+    print ('#define INCLUDED_FILES "{}"'.format (included_files))
     print ('included files:', included_files, file = sys.stderr)
 
     sys.exit (0)
--- tests/includes_t.c
+++ tests/includes_t.c
@@ -1,85 +1,24 @@
+#define _GNU_SOURCE
+
+#include <stdio.h>
 #include "includes_t.h"
-
-#include "../bin/license-header/check_file.c"
-#include "../bin/license-header/walkdir.c"
-#include "../bin/jcasdb/usage.c"
-#include "../bin/jcasdb/dbinfo.c"
-#include "../bin/jcaslib/print_build_info.c"
-#include "../bin/jcaslib/print_version.c"
-#include "../lib/cfg/cfg.c"
-#include "../lib/lib/xrealloc.c"
-#include "../lib/lib/xmalloc.c"
-#include "../lib/path/path_islink.c"
-#include "../lib/path/path_isdir.c"
-#include "../lib/path/path_exists.c"
-#include "../lib/path/path_isfile.c"
-#include "../lib/path/path_join.c"
-#include "../lib/path/path_filename_ext.c"
-#include "../lib/test/t_end.c"
-#include "../lib/test/t_fatal.c"
-#include "../lib/test/t_error.c"
-#include "../lib/test/t_log.c"
-#include "../lib/test/t_check.c"
-#include "../lib/test/t_debug.c"
-#include "../lib/test/tsuite_end.c"
-#include "../lib/test/t_start.c"
-#include "../lib/test/_tinfo.c"
-#include "../lib/test/_tfail.c"
-#include "../lib/test/_tprint.c"
-#include "../lib/test/_tpass.c"
-#include "../lib/test/tsuite_start.c"
-#include "../lib/os/os_lsdir.c"
-#include "../lib/os/_os_scandir.c"
-#include "../lib/log/log_exit.c"
-#include "../lib/log/log_warn.c"
-#include "../lib/log/_logfp.c"
-#include "../lib/log/log_set_progname.c"
-#include "../lib/log/log_dbg.c"
-#include "../lib/log/log_err.c"
-#include "../lib/log/log_print.c"
-#include "../lib/db/_db_updated.c"
-#include "../lib/db/db_create.c"
-#include "../lib/db/db_delete.c"
-#include "../lib/db/db_close.c"
-#include "../lib/db/db_fetch.c"
-#include "../lib/db/_db_init.c"
-#include "../lib/db/db_replace.c"
-#include "../lib/db/_db_open.c"
-#include "../lib/db/_db_store.c"
-#include "../lib/db/db_open.c"
-#include "../lib/db/db_insert.c"
-#include "../lib/db/db_fetchall.c"
-#include "../lib/db/dbdata_free.c"
-#include "../lib/str/str_array_alloc.c"
-#include "../lib/str/str_array_get.c"
-#include "../lib/str/str_alloc.c"
-#include "../lib/str/str_nset.c"
-#include "../lib/str/str_set.c"
-#include "../lib/str/str_array_free.c"
-#include "../lib/str/str_array_append.c"
-#include "../lib/str/str_join.c"
-#include "../lib/str/str_vjoin.c"
-#include "../lib/str/str_realloc.c"
-#include "../lib/str/str_free.c"
-#include "../lib/str/str_array_insert.c"
-#include "../lib/str/str_array_realloc.c"
-#include "../lib/str/str_cat.c"
-#include "../lib/str/str_ncat.c"
-
+#ifndef INCLUDED_FILES
+#define INCLUDED_FILES "-1"
+#endif
 #include <jcaslib/test.h>
 
 void includes_t (test_suite_T *ts) {
-  int expect = 1;
-  test_T *t = t_start (ts, "65 files", expect);
-  t_check (t, 0, "");
-  t_end (t);
+    int expect = 1;
+    test_T *t = t_start (ts, INCLUDED_FILES " file(s)", expect);
+    t_check (t, 0, "");
+    t_end (t);
 }
 
 int main (int argc, char *argv[]) {
-  if (argc < 1)
+    if (argc < 1)
     errx (1, "ERROR: argc < 1???");
-  int expect = 1;
-  test_suite_T *ts = tsuite_start (argv[0], expect);
-  includes_t (ts);
-  return (tsuite_end (ts));
+    int expect = 1;
+    test_suite_T *ts = tsuite_start (argv[0], expect);
+    includes_t (ts);
+    return (tsuite_end (ts));
 }
