a4eb1da3083f542a2f3bcd7fffdf750e7515549e
Jeremías <jrmsdev@gmail.com>
2017-05-22 20:39:27 -0300

    disable apt run via cmd args

--- scripts/ci/before.sh
+++ scripts/ci/before.sh
@@ -4,12 +4,13 @@ CC=${CC:-"gcc"}
 CHECK_VG=${CHECK_VG:-"false"}
 
 export DEBIAN_FRONTEND=noninteractive
-apt-get -qq update && apt-get -qq -y $APTARGS install $DEBPKGS $CC
 
-test "true" = "$CHECK_VG" && apt-get -qq -y $APTARGS install valgrind
+if test "--no-apt" != "$1"; then
+    apt-get -qq update && apt-get -qq -y $APTARGS install $DEBPKGS $CC
+    test "true" = "$CHECK_VG" && apt-get -qq -y $APTARGS install valgrind
+fi
 
 $CC --version
-
 test "true" = "$CHECK_VG" && valgrind --version
 
 bmake distclean
